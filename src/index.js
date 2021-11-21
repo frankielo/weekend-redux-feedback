56696365a568cac95ece0fe62c8ac0cd63d9626a
Francisco Lopez <frankie.b.lopez@gmail.com>
2021-11-21 15:29:32 -0600

    setup my reducer inside index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {createStore,combineReducers} from 'redux'
+import {Provider} from 'react-redux'
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const addDataReducer = (state=[], action)=>{
+    if(action.type === 'ADD_DATA'){
+        return [...state, acation.payload]
+    }
+    else if(action.type === 'CLEAR_REDUCER'){
+        return []
+    }
+    return state
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        addDataReducer
+    })
+)
+
+ReactDOM.render(
+<Provider store={storeInstance}>
+ <App />
+</Provider>
+, document.getElementById('root'));
 registerServiceWorker();
